9e86f38a5860c8d7c692938b47dedb6f885b3929
Xinze Zhang <xinze@hust.edu.cn>
2018-06-24 22:32:24 +0800

    stop experiment

--- FasionMNIST_pytorch_mT/Model_Run/SCSF_C192.ps1
+++ FasionMNIST_pytorch_mT/Model_Run/SCSF_C192.ps1
@@ -1,12 +1,5 @@
-Write-Host "============================================================"
-Write-Host "python SCSF.py --n_kernel 192 --k_allTrain_epochs 1"
-Write-Host "============================================================"
-python SCSF.py --n_kernel 192 --n_kernel 192 --k_allTrain_epochs 1 
-Write-Host "============================================================"
-Write-Host "python SCSF.py --n_kernel 192 --k_allTrain_epochs 5"
-Write-Host "============================================================"
-python SCSF.py --n_kernel 192 --k_allTrain_epochs 5 
-for($i=0; $i -le 300; $i=$i+10)
+
+for($i=110; $i -le 300; $i=$i+10)
 {
     Write-Host "============================================================"
     Write-Host "python SCSF.py --n_kernel 192 --k_allTrain_epochs  $i "
