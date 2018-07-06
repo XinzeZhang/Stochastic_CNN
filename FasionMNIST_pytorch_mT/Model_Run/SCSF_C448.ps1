264dac20c2786452dd5e03e554451c8a5aed9de5
Xinze Zhang <xinze@hust.edu.cn>
2018-07-06 17:17:52 +0800

    updated results of C512 C576

--- FasionMNIST_pytorch_mT/Model_Run/SCSF_C448.ps1
+++
@@ -1,8 +0,0 @@
-for($i=180; $i -le 300; $i=$i+10)
-{
-    Write-Host "============================================================"
-    Write-Host "python SCSF.py --n_kernel 448 --k_allTrain_epochs  $i "
-    Write-Host "============================================================"
-    python SCSF.py --n_kernel 448 --k_allTrain_epochs $i 
-}
-
